f85d079b3d2e602f3d9be8159ec190bf48ba8ea8
Reulisson <115098329+Reulisson@users.noreply.github.com>
2022-12-13 20:59:07 -0300

    Delete vetores.h

--- Projeto Final LP2022-2/vetores.h
+++
@@ -1,8 +0,0 @@
-#define TAM 20
-#define N 7
-
-void preencherVetor(int *pv, int qtd);
-void imprimirVetor(int *pv, int qtd);
-void imprimirMatriz(int *pm, int dim);
-void matrizCoocorrencias(int *px, int *py, int qtd, int *pm, int dim);
-void vetorizacao(int *pv, int *pm, int qnl, int qnc);
